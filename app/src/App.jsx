b08a7d977166eb209d09b110e7f555a328eacccf
suman Mishra <kmsuman27@gmail.com>
2026-08-30 00:47:18 +0530

    feat: add initial react docker terraform setup

---
+++ app/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>React DevOps Assessment</h1>
+
+      <p>Application deployed successfully 🚀</p>
+
+      <p>Environment: Production</p>
+
+      <p>Version: 1.0.0</p>
+    </div>
+  )
+}
+
+export default App
