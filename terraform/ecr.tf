c9d87dac15688b39fce312ada9eb8c4f77571cc3
suman Mishra <kmsuman27@gmail.com>
2026-08-30 18:39:43 +0530

    deploy application

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,6 +2,8 @@ resource "aws_ecr_repository" "react_app" {
   name                 = "react-devops"
   image_tag_mutability = "MUTABLE"
 
+  force_delete = true
+
   image_scanning_configuration {
     scan_on_push = true
   }
